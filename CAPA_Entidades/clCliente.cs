404197995427050a0a4e4a4acd11372e157ec877
miguel martinez <ingmartinez7520@hotmail.com>
2020-03-05 00:18:36 -0500

    tarabajando en la tabla entidades entrando los datos de los autores.

---
+++ CAPA_Entidades/clCliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPA_Entidades
+{
+    class clCliente
+    {
+        private int idcliente;
+        private string Nombre;
+        private string Direccion;
+        private string Telefono;
+        private string Ciudad;
+        private string Cedula;
+        private string TipoCliente;
+        private string Email;
+
+        public int Idcliente
+            { get => idcliente; set => idcliente = value; }
+             
+       //ublic int 
+    }
+}
