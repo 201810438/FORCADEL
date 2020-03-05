d50459a94464448c096f4585c7fb774e50e4de1e
miguel martinez <ingmartinez7520@hotmail.com>
2020-03-05 14:46:49 -0500

    realizada la clase de conexion en la CAPA_Datos

---
+++ CAPA_Datos/cl_Conexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CAPA_Datos
+{
+   public class cl_Conexion
+    {
+        SqlConnection conexion = new SqlConnection("Data Source =.; Initial Catalog = inventariop; Integrated Security = True");
+
+        //metodo para abrir la conexion//
+        void abrir_conexion()
+        {
+            if (conexion.State==ConnectionState.Closed)
+            
+                conexion.Open();
+        }
+        //metodo para cerrar la conexion
+        void cerrar_conexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+        }
+       
+
+    }
+}
